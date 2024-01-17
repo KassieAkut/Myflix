863430177d2d3dc4d7d2eeaed1f590a7de0f262a
Cassandra Akut <82506170+KassieAkut@users.noreply.github.com>
2024-01-17 15:09:07 +0000

    Update settings.py

--- clone/settings.py
+++ clone/settings.py
@@ -142,7 +142,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CSRF_TRUSTED_ORIGINS = ['https://cassidymyflix.net']
+CSRF_TRUSTED_ORIGINS = ['http://cassidymyflix.net']
 CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
